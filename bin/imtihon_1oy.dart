07efe75553d5b21e8d20292f86c89774378f761a
Akmaljon Abdirakhimov <AkmaljonAbdirakhimov@gmail.com>
2024-04-23 09:44:12 +0500

    first commit

---
+++ bin/imtihon_1oy.dart
@@ -0,0 +1,5 @@
+import 'package:imtihon_1oy/imtihon_1oy.dart' as imtihon_1oy;
+
+void main(List<String> arguments) {
+  print('Hello world: ${imtihon_1oy.calculate()}!');
+}
---
+++ test/imtihon_1oy_test.dart
@@ -0,0 +1,8 @@
+import 'package:imtihon_1oy/imtihon_1oy.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
